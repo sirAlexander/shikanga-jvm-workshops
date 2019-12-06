cec95c3c2b2325808a979305c03d0e2031082fbd
sirAlexander <alexander.shikanga.tindi@gmail.com>
2019-12-06 21:52:05 +0000

    initial commit of `shikanga's super-heroes` quarkus microservice

---
+++ shikanga-quarkus-super-heroes-workshop/shikanga-super-heroes/rest-hero/src/test/java/uk/co/shikanga/workshop/superheroes/hero/NativeHeroResourceIT.java
@@ -0,0 +1,9 @@
+package uk.co.shikanga.workshop.superheroes.hero;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeHeroResourceIT extends HeroResourceTest {
+
+    // Execute the same tests but in native mode.
+}
