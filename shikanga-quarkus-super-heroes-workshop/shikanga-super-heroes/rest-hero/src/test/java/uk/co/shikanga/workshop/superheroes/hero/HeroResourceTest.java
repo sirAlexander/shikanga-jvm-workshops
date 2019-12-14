638859c8f0b428c85036b6ffdfe656c8d8d0b497
sirAlexander <alexander.shikanga.tindi@gmail.com>
2019-12-14 23:51:30 +0000

    updated to support CRUD tests. Introduced `TestContainers` for running tests

--- shikanga-quarkus-super-heroes-workshop/shikanga-super-heroes/rest-hero/src/test/java/uk/co/shikanga/workshop/superheroes/hero/HeroResourceTest.java
+++ shikanga-quarkus-super-heroes-workshop/shikanga-super-heroes/rest-hero/src/test/java/uk/co/shikanga/workshop/superheroes/hero/HeroResourceTest.java
@@ -1,13 +1,39 @@
 package uk.co.shikanga.workshop.superheroes.hero;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
+@Testcontainers
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class HeroResourceTest {
+
+    @Container
+    private static final PostgreSQLContainer DATABASE = new PostgreSQLContainer<>("postgres:10.5")
+            .withDatabaseName("heroes_database")
+            .withUsername("superman")
+            .withPassword("superman")
+            .withExposedPorts(5432);
+
+    @BeforeAll
+    private static void configure() {
+        System.setProperty("quarkus.datasource.url", DATABASE.getJdbcUrl());
+    }
+
+    @AfterAll
+    private static void cleanup() {
+        System.clearProperty("quarkus.datasource.url");
+    }
 
     @Test
     public void testHelloEndpoint() {
